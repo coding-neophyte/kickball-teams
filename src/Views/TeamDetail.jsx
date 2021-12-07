ba936acb45a981a74daf51db1603aef7d45cc5f2
Julius Franklin <jjfranklin88@outlook.com>
2021-12-06 22:56:58 -0500

    logic for team detail

--- src/Views/TeamDetail.jsx
+++ src/Views/TeamDetail.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { Link } from 'react-router-dom'
+import { getTeamById } from '../Services/teams'
 
 export default function TeamDetail() {
+    const [team, setTeam] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const { teamId } = useParams()
+
+    useEffect(() => {
+        getTeamById(teamId)
+        .then((resp) => setTeam(resp))
+        .finally(() => setIsLoading(false))
+    }, [teamId])
+
+    if(isLoading) return <h1> Loading...</h1>
+
     return (
         <div>
-            
+            <p>
+            <Link to='/teams'> Back to Team List </Link>
+            </p>
+            <h1>{team.name}</h1>
+            <p> {team.city} {team.state}</p>
         </div>
     )
 }
